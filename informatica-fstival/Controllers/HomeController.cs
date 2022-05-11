5c91410ccfe7387d2f6630489f4a96f6b602f06a
Nine van de Sande <110717@st-maartenscollege.nl>
2022-05-11 10:45:08 +0200

    foutmeldingen weggehaald

--- informatica-fstival/Controllers/HomeController.cs
+++ informatica-fstival/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using informatica_fstival.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 
